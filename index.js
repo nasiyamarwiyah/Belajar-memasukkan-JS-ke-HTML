61fe6ae670f3d09a9d9b8010e9fdfbfebdedd9a6
nasiyamarwiyah <nasiyamarwiyah04@gmail.com>
2023-07-18 11:27:44 +0700

    belajar JS

---
+++ index.js
@@ -0,0 +1,10 @@
+function warning() {
+    let angkaPertama = document.getElementById("angka-1").value
+    let angkaKedua = document.getElementById("angka-2").value
+    let hasil = parseInt(angkaPertama) + parseInt(angkaKedua)
+    console.log(hasil)
+}
+
+
+​
+
